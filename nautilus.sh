ddbb64f5aa25e6ab3fde955e744b5235f0d34b6f
Kuan Fan <kuan.fan@gov.bc.ca>
2026-01-21 14:23:16 -0800

    add oc login

--- nautilus.sh
+++ nautilus.sh
@@ -6,6 +6,8 @@ cd charts/nautilus-test-app
 
 pwd
 
+oc login --token="${OPENSHIFT_TOKEN}" --server=https://api.silver.devops.gov.bc.ca:6443
+
 oc projects
 
 helm -n e52f12-dev upgrade --install hackathon-sample-app .

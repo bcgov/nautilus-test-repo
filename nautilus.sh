e8c631d705bc00d4a7e221f8fd04ec0a6cb25916
Kuan Fan <kuan.fan@gov.bc.ca>
2026-01-21 13:13:04 -0800

    add helm chart

--- nautilus.sh
+++ nautilus.sh
@@ -1,3 +1,9 @@
 set -euxo pipefail
 
+echo "Going to deploy the sample app!"
+
+cd charts/nautilus-test-app
+
+helm -n e52f12-dev upgrade --install hackathon-sample-app .
+
 echo "Deployed! develop branch"

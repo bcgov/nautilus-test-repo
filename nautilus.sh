aab7efb201444d70abbbbd3e8c51ea7a823ef84d
Kuan Fan <kuan.fan@gov.bc.ca>
2026-01-21 15:30:14 -0800

    update deploy

--- nautilus.sh
+++ nautilus.sh
@@ -4,11 +4,7 @@ echo "Going to deploy the sample app!"
 
 cd charts/nautilus-test-app
 
-pwd
-
 oc login --token="${OPENSHIFT_TOKEN}" --server=https://api.silver.devops.gov.bc.ca:6443
-
-oc projects
 
 if helm -n e52f12-dev status hackathon-2026 >/dev/null 2>&1; then
   oc -n e52f12-dev rollout restart deployment hackathon-2026-nautilus-test-app

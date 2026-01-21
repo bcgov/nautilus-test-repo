d22c0eea1f0089b14f6014055ab589d79fe158a7
Kuan Fan <kuan.fan@gov.bc.ca>
2026-01-21 15:24:54 -0800

    only install if not exist

--- nautilus.sh
+++ nautilus.sh
@@ -10,8 +10,10 @@ oc login --token="${OPENSHIFT_TOKEN}" --server=https://api.silver.devops.gov.bc.
 
 oc projects
 
-helm -n e52f12-dev upgrade --install hackathon-2026 .
-
-oc -n e52f12-dev rollout restart deployment hackathon-2026-nautilus-test-app
+if helm -n e52f12-dev status hackathon-2026 >/dev/null 2>&1; then
+  oc -n e52f12-dev rollout restart deployment hackathon-2026-nautilus-test-app
+else
+  helm -n e52f12-dev upgrade --install hackathon-2026 .
+fi
 
 echo "Deployed! develop branch"
